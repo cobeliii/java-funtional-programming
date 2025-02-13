93457b5add6529886dc71efd5d760102b8c1038c
cobeliii <cobeliiimuname@gmail.com>
2025-02-13 00:37:33 -0600

    Made some changes to Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,18 @@ public class Main {
     public static void main(String[] args) {
         EmailSender sender = new Gmail();
         sender.send();
+
+        EmailSender emailSender = new EmailSender() {
+            @Override
+            public void send() {
+                System.out.println("Sending email in a different way");
+            }
+        };
+
+        emailSender.send();
+
+        //Using lambda now
+        EmailSender emailSender2 = () -> System.out.println("Sending email in a different way part 2");
+        emailSender2.send();
     }
 }
